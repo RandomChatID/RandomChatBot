ef790b6757a397563022a808ac34d2660fb020b3
Kucekmill <79855424+kucekmill@users.noreply.github.com>
2021-11-29 20:11:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def handle(update):
 				queue["free"].append(uid)
 
 		if text == "/help":
-			bot.sendMessage(uid, "Help:\n\nMenggunakan /start untuk mulai mencari pasangan percakapan, setelah anda cocok, anda dapat menggunakan /end untuk mengakhiri percakapan.\n\nIf you have any questions or require help, join @sadnesstalk or ask @sadnesstalk.\n@antigabutbrothers")
+			bot.sendMessage(uid, "Help:\n\nMenggunakan /start untuk mulai mencari pasangan percakapan, setelah anda cocok, anda dapat menggunakan /end untuk mengakhiri percakapan.\n\nJika Anda memiliki pertanyaan atau memerlukan bantuan, join @sadnesstalk atau bertanya @sadnesstalk.\n@antigabutbrothers")
 
 		if text == "/nopics":
 			config[str(uid)]["pics"] = not config[str(uid)]["pics"] 
